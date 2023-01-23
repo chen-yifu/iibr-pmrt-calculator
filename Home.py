87835b39bdf10461d4f8d44782354ccfec78cb1f
Yifu Chen <yifu.chen@alumni.ubc.ca>
2023-01-22 17:52:17 -0800

    change step size of slider to 0.1

--- Home.py
+++ Home.py
@@ -88,7 +88,7 @@ with st.form(key='my_form'):
             val = st.checkbox(label=label, key=col_name)
         elif dtype == "real":
             avg_val = col_to_avg[col_name]
-            val = st.slider(label=label, key=col_name, max_value=100.0, step=0.01, value=avg_val)
+            val = st.slider(label=label, key=col_name, max_value=100.0, step=0.1, value=avg_val)
             html_str = f"<span style='color:grey'>If the information is missing or unknown, you could choose to use the average value in dataset ({col_to_avg[col_name]:.3f}) or provide a best guess. </span>"
             st.markdown(html_str, unsafe_allow_html=True)
         elif dtype == "integer":
